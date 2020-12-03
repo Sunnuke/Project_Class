ac20035d646b08a9db74a1a16a46495cc08b7736
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-02 17:27:33 -0800

    Complete basic layout of all member variables and methods for the Project class

--- Project.java
+++ Project.java
@@ -1,13 +1,39 @@
+import java.util.Random;
 
 class Project {
-    
+    // Attributes
     private String name;
     private String description;
 
-    public elevatorPitch() {}
+// Details
+    public String elevatorPitch() {
+        String name = this.name;
+        if(this.description.length() > 0) {
+            String description = this.description;
+            return name + ": " + description;
+        }
+        else {
+            return name + ": No description was given!";
+        }
+    }
 
-    public Project() {}
-    public Project(String name) {}
-    public Project(String name, String description) {}
-    public getProject() {}
+    // Methods
+    public Project() {
+        this("Project"+((int)(Math.random() * 99999 - 10000) + 10000));
+    }
+    public Project(String name) {
+        this.name =name;
+    }
+    public Project(String name, String description) {
+        this.name =name;
+        this.description =description;
+    }
+
+    // Project "gets" 
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
 }
