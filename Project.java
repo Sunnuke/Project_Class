2f98dd1029099f3c631d1ea9ef0b8ced44c0c03e
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-02 16:38:27 -0800

    Created Project class and setup basic member variables and methods for the Project class

---
+++ Project.java
@@ -0,0 +1,13 @@
+
+class Project {
+    
+    private String name;
+    private String description;
+
+    public elevatorPitch() {}
+
+    public Project() {}
+    public Project(String name) {}
+    public Project(String name, String description) {}
+    public getProject() {}
+}
